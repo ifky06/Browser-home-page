5d97ac7c10b15b89173bd1266f2596fb469eede6
Ahmad Rifki Fauzi <ahmadrifki3403@gmail.com>
2023-03-05 12:09:20 +0700

    Make Weather Feel Better

--- src/Weather/Weather.jsx
+++ src/Weather/Weather.jsx
@@ -1,4 +1,5 @@
 import React,{useState,useEffect} from "react";
+import { BsDropletFill, BsWind } from 'react-icons/bs';
 
 const Weather = () => {
     const [data, setData] = useState({});
@@ -14,15 +15,51 @@ const Weather = () => {
         fetchData();
     }, []);
 
-    return (
-        <div>
-            <h1>Weather in malang:</h1>
+    const weather = () => {
+        if(!data){
+            return (
+            <>
+                <div className="spinner-border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </div>
+                Loading Masbro..
+            </>
+            );
+        }
 
-                <p>Temperature: {data.temp_c} °C</p>
-                <p>Condition: {data.condition ? data.condition.text:'-'}</p>
-                <img src={data.condition ? data.condition.icon:'-'}/>
-                <p>Humidity: {data.humidity}%</p>
-                <p>Wind: {data.wind_kph} km/h</p>
+        return (
+            <>
+                <div className="container">
+                    <div className="row pb-0 mb-0 ">
+                        <p className="pt-1 m-0 pb-0 fw-bold fs-4">Malang</p>
+                    </div>
+                    <div className="row m-0 ">
+                        <div className="col-6 text-end p-0 mb-0">
+                            <img className="mb-0" src={data.condition ? data.condition.icon:'-'} alt=""/>
+                        </div>
+                        <div className="col-6 text-start p-0 pt-1 mb-0 d-flex">
+                            <h1 className="fw-bolder">{data.temp_c}</h1>
+                            <span className="fw-bolder">°C</span>
+                        </div>
+                    </div>
+                    <div className="row text-start ps-4">
+                        <p className="my-0">{data.condition ? data.condition.text:'-'}</p>
+                        <span className="my-0">
+                            <BsDropletFill className="my-0" size={10}/> {data.humidity}%
+                        </span>
+                        <span className="my-0">
+                            <BsWind className="my-0" size={10}/> {data.wind_kph} km/h
+                        </span>
+
+                    </div>
+                </div>
+            </>
+        );
+    }
+
+    return (
+        <div className="col-md-6 pt-3">
+            {weather()}
         </div>
     );
 }
