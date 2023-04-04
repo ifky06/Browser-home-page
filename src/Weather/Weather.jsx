174a9f1ba29489cb20fb2be21c1e6b6c94151279
Ahmad Rifki Fauzi <ahmadrifki3403@gmail.com>
2023-04-04 21:25:51 +0700

    Fixed style weather

--- src/Weather/Weather.jsx
+++ src/Weather/Weather.jsx
@@ -28,39 +28,36 @@ const Weather = () => {
         }
 
         return (
-            <>
-                <div className="container">
-                    <div className="row pb-0 mb-0 ">
-                        <p className="pt-1 m-0 pb-0 fw-bold fs-4">Malang</p>
+            <div className="d-flex flex-column justify-content-center mt-3" style={{width:'65%'}}>
+                <div className="row ">
+                    <p className="pt-1 m-0 pb-0 fw-bold fs-4">Malang</p>
+                </div>
+                <div className="row m-0">
+                    <div className="col-6 text-end p-0 mb-0">
+                        <img className="mb-0" src={data.condition ? data.condition.icon : '-'} alt=""/>
                     </div>
-                    <div className="row m-0 ">
-                        <div className="col-6 text-end p-0 mb-0">
-                            <img className="mb-0" src={data.condition ? data.condition.icon:'-'} alt=""/>
-                        </div>
-                        <div className="col-6 text-start p-0 pt-1 mb-0 d-flex">
-                            <h1 className="fw-bolder">{data.temp_c}</h1>
-                            <span className="fw-bolder">°C</span>
-                        </div>
-                    </div>
-                    <div className="row text-start ps-4">
-                        <p className="my-0">{data.condition ? data.condition.text:'-'}</p>
-                        <span className="my-0">
-                            <BsDropletFill className="my-0" size={10}/> {data.humidity}%
-                        </span>
-                        <span className="my-0">
-                            <BsWind className="my-0" size={10}/> {data.wind_kph} km/h
-                        </span>
-
+                    <div className="col-6 text-start p-0 pt-1 mb-0 d-flex">
+                        <h1 className="fw-bolder">{data.temp_c}</h1>
+                        <span className="fw-bolder">°C</span>
                     </div>
                 </div>
-            </>
+                <div className="row ps-3 d-flex flex-row justify-content-center w-100">
+                    {/*<p className="my-0">{data.condition ? data.condition.text:'-'}</p>*/}
+                    <span className="m-0 w-auto">
+                            <BsDropletFill className="my-0" size={10}/> {data.humidity}%
+                        </span>
+                    <span className="m-0 w-auto">
+                            <BsWind className="my-0" size={10}/> {data.wind_kph} km/h
+                        </span>
+                </div>
+            </div>
         );
     }
 
     return (
-        <div className="col-md-6 pt-3">
+        <>
             {weather()}
-        </div>
+        </>
     );
 }
 
